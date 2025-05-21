1f95fe2343397482b7b28951d4139db8ec17a1bd
Lupe <refugiocarrillonunez@gmail.com>
2025-05-20 23:36:03 -0600

    inicie con la pantalla principal

--- app/src/main/java/com/example/rutea/MainActivity.java
+++ app/src/main/java/com/example/rutea/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.rutea;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsetsController;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +25,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Window window = getWindow();
+
+        // Cambiar color de la barra de estado
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.verde));
+            window.setNavigationBarColor(ContextCompat.getColor(this, R.color.verde));
+        }
+
+        // Para que los íconos de la barra se vean blancos (en lugar de oscuros)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().getInsetsController().setSystemBarsAppearance(
+                    0,
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS | WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+            );
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // API 23-29
+            View decor = getWindow().getDecorView();
+            decor.setSystemUiVisibility(0); // Remueve íconos oscuros
+        }
+
     }
 }
